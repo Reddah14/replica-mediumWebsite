237e7f4891385a92bf0f3acdc86f78b269734810
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-11 23:35:59 +0200

    small fix banner component

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,12 +8,12 @@ const Banner = () => {
         <h3 className="col-8">Programming</h3>
         <button type="button" class=" col-4 btn btn-success follow-btn">Follow</button>
       </div>
-      <div className="row banner__topic-subtitle">
-        Quote about the topic OR topic sub-title
+      <div className="col-12 banner__topic-subtitle">
+        The good, the bad, the buggy.
       </div>
       <div className="row banner__topic-description">
         <p>
-          Follow Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus nobis.
+          Follow Programming topic because Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus nobis.
         </p>
       </div>
     </div>
