717434260cb1e6776d3910b33c32fb41737d64b4
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-11 23:26:48 +0200

    banner jsx and css first approach done (mobile screen only)

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,8 +3,19 @@ import "./Banner.scss";
 
 const Banner = () => {
   return (
-    <div className="banner">
-      Banner
+    <div className="row banner">
+      <div className="row banner__topic-title">
+        <h3 className="col-8">Programming</h3>
+        <button type="button" class=" col-4 btn btn-success follow-btn">Follow</button>
+      </div>
+      <div className="row banner__topic-subtitle">
+        Quote about the topic OR topic sub-title
+      </div>
+      <div className="row banner__topic-description">
+        <p>
+          Follow Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus nobis.
+        </p>
+      </div>
     </div>
   )
 }
