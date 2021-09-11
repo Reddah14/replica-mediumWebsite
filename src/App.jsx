b0745e2d33e96e4690b505a65cba65b8377199a0
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-11 20:06:54 +0200

    added NewsArticle component  in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import React from 'react';
+
 import './App.scss';
 import Nav from './components/Nav/Nav';
+import NewsArticle from './components/NewsArticle/NewsArticle';
 
 function App() {
   return (
     <>
       <Nav />
+      <NewsArticle />
     </> 
   )
 }
