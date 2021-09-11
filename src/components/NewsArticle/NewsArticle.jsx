c35e3cc9614207447e9bcb46f44ca2dab0b70bbd
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-11 21:50:15 +0200

    adding NewsArticle component

--- src/components/NewsArticle/NewsArticle.jsx
+++ src/components/NewsArticle/NewsArticle.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+
 import "./NewsArticle.scss";
 
 const NewsArticle = () => {
-  return <p>NewsArticle</p>;
+  return (
+    <article className="row news-article">
+      <div className="col-8">
+        <h3 className="news-article__header">Header</h3>
+        <p className="news-article__description">...description...</p>
+      </div>
+      <div className="col-4 news-article__image">
+        <img src="" alt="" />
+      </div>
+    </article>
+  )
 };
 
 export default NewsArticle;
