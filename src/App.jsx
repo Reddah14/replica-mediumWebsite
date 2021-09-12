97be65bcd8fab011085d611625835f9021ef10ba
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-12 18:28:44 +0200

    fix mocked data to look more real

--- src/App.jsx
+++ src/App.jsx
@@ -9,36 +9,53 @@ import MainArticle from "./components/MainArticle/MainArticle";
 
 const news = [
   {
-    id: "e1",
-    title: "Toilet Paper Rises price !",
+    id: "e0",
+    title: "Map, Filter, and Reduce in Swift",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore pariatur...",
+      "How to use your curiosity and Chrome DevTools to learn more about web...",
+    author: "Margarita Fox",
+    imageURL: "https://www.stevensegallery.com/140/200",
+    readTime: 8,
+    date: "Set 8",
+  },
+  {
+    id: "e1",
+    title: "Write your tools: simple JIRA automation",
+    description:
+      "Cloud-based solutions are the new future for programmers and the data scientists...",
     author: "Billie Jean",
     imageURL: "http://www.fillmurray.com/200/300",
+    readTime: 4,
+    date: "Set 10",
   },
   {
     id: "e2",
-    title: "New TV",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore pariatur...",
+    title: "Kickstart with Micronaut Framework",
+    description: "Fun Python programs explained with code...",
     author: "Bruce Willis",
     imageURL: "http://www.fillmurray.com/g/200/300",
+    readTime: 5,
+    date: "Set 11",
   },
   {
     id: "e3",
-    title: "Car Insurance",
+    title: "6 Must-try Python Programse",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore pariatur...",
+      "It’s important not to put all your eggs into one basket. That’s why despite your main job it’s crucial you have a side gig. In this...",
     author: "Madonna Smith",
     imageURL: "https://placebear.com/200/300",
+    readTime: 3,
+    date: "Set 11",
   },
   {
     id: "e4",
-    title: "New Desk (Wooden)",
+    title: "How to execute an object file: Part 3",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore pariatur...",
+      "It’s been more than 11 years in my career in the IT industry. I have worked in different teams of different sizes and of different...",
     author: "John Doe",
     imageURL: "https://www.stevensegallery.com/g/200/300",
+    readTime: 7,
+    date: "Set 12",
   },
 ];
 
