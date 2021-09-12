1ef7af6b1ae8ffda1b765503214bdcdf048b4998
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-12 16:58:29 +0200

    adding mocked links to navbar icons

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -9,18 +9,21 @@ const Nav = () => {
     <div className="row nav">
       <div className="col-5 nav__items">
         <img className="nav__items--logo" alt="ball logo" src={logo} />
-        <a
-          className="nav__items--link"
-          href="https://www.placecage.com/"
-        >
+        <a className="nav__items--link" href="https://www.placecage.com/">
           Open in app
         </a>
       </div>
       <div className="col-3"></div>
       <div className="col-4 nav__functions">
-        <a href="https://placebear.com/"><FaSearch /></a>
-        <a href="http://www.fillmurray.com/"><FaBell /></a>
-        <a href="https://www.stevensegallery.com/"><FaUserCircle /></a>
+        <a href="https://placebear.com/">
+          <FaSearch />
+        </a>
+        <a href="http://www.fillmurray.com/">
+          <FaBell />
+        </a>
+        <a href="https://www.stevensegallery.com/">
+          <FaUserCircle />
+        </a>
       </div>
     </div>
   );
