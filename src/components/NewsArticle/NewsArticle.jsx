2779be8c7a811d47ad85b50f860b862cef1a8b46
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-12 14:39:30 +0200

    refactor JSX for newsArticle component

--- src/components/NewsArticle/NewsArticle.jsx
+++ src/components/NewsArticle/NewsArticle.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 
 import "./NewsArticle.scss";
+import { FaBookmark } from "react-icons/fa";
 
-const NewsArticle = () => {
+
+const NewsArticle = (props) => {
   return (
     <article className="row news-article">
       <div className="col-8">
-        <h3 className="news-article__header">Header</h3>
-        <p className="news-article__description">...description...</p>
+        <h3 className="news-article__header">{props.title}</h3>
+        <p className="news-article__description">{props.description}</p>
       </div>
       <div className="col-4 news-article__image">
-        <img src="" alt="" />
+      </div>
+      <div className="row news-footer">
+          <div
+            className="col-11">{props.author}
+            <div> 12 Sep · 5 min read</div>          
+          </div>
+          <div className="col-1 bookmark-icon"><FaBookmark /></div>
       </div>
     </article>
   )
