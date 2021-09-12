416ab015c22cf8b2a160d3cfc9597a12dc6e7a89
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-12 17:15:37 +0200

    adding component for main new

---
+++ src/components/MainArticle/MainArticle.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./MainArticle.scss";
+
+const MainArticle = () => {
+  return (
+    <div className="row main-article">
+      Main article
+    </div>
+  );
+}
+
+export default MainArticle;
