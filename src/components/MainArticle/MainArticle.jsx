1474475e972cf1d6461b28041c1387eb562066e2
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-12 18:29:47 +0200

    adding Main Article component

--- src/components/MainArticle/MainArticle.jsx
+++ src/components/MainArticle/MainArticle.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 
 import "./MainArticle.scss";
+import { FaUserCircle } from "react-icons/fa";
 
-const MainArticle = () => {
+
+const MainArticle = (props) => {
   return (
-    <div className="row main-article">
-      Main article
-    </div>
+    <article className="row main-article">
+      <img src={props.imageUrl} className="col-12 main-article__image" alt="murray" />
+      <div className="col-12">
+        <h3 className="main-article__header">{props.title}</h3>
+        <p className="col-12 main-article__description">{props.description}</p>
+      </div>
+      <div className="row main-footer">
+        <div className="col-2 main-footer__profile">
+          <FaUserCircle />
+        </div>
+        <div className="col-8 main-footer__info">
+          <div className="row main-footer__info--author">
+            {props.author}
+          </div>
+          <div className="row main-footer__info--time">
+            {props.date} · {props.readTime} min read
+          </div>
+        </div>
+      </div>
+    </article>
   );
 }
 
