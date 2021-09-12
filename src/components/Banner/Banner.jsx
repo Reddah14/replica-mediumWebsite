f3711da675f07a455cd9d621bee0e92f751a484b
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-12 18:29:14 +0200

    small fixes

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
     <div className="row banner">
       <div className="row banner__topic-title">
         <h3 className="col-8">Programming</h3>
-        <button type="button" class=" col-4 btn btn-success follow-btn">Follow</button>
+        <button type="button" className=" col-4 btn btn-success follow-btn">Follow</button>
       </div>
       <div className="col-12 banner__topic-subtitle">
         The good, the bad, the buggy.
