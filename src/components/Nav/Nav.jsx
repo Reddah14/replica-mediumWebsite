d5a2596b321be9fc586c3411d4680908cbc76649
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-12 12:54:43 +0200

    refactoring nav JSX

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,28 @@
 import React from "react";
+
 import "./Nav.scss";
+import logo from "../../assets/images/logo.png";
+import { FaBell, FaSearch, FaUserCircle } from "react-icons/fa";
 
 const Nav = () => {
   return (
-    <div className="row">
-      <p className="col-4">nav</p>
-      <p className="col-4">nav</p>
-      <p className="col-4">nav</p>
+    <div className="row nav">
+      <div className="col-5 nav__items">
+        <img className="nav__items--logo" alt="ball logo" src={logo} />
+        <a
+          href="https://www.placecage.com/"
+        >
+          Open in app
+        </a>
+      </div>
+      <div className="col-3"></div>
+      <div className="col-4 nav__functions">
+        <a href="https://placebear.com/"><FaSearch /></a>
+        <a href="http://www.fillmurray.com/"><FaBell /></a>
+        <a href="https://www.stevensegallery.com/"><FaUserCircle /></a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Nav;
