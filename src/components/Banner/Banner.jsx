585a92f730deb3c88e0666b0cacb0b7fce4740af
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-13 16:24:06 +0200

    using props at banner component

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 import "./Banner.scss";
 
-const Banner = () => {
+const Banner = (props) => {
   return (
     <div className="row banner">
       <div className="row banner__topic-title">
-        <h3 className="col-8">Programming</h3>
+        <h3 className="col-8">{props.info[0].title}</h3>
         <button type="button" className=" col-4 btn btn-success follow-btn">
           Follow
         </button>
       </div>
       <div className="col-12 banner__topic-subtitle">
-        The good, the bad, the buggy.
+        {props.info[0].subtitle}
       </div>
       <div className="row banner__topic-description">
         <p>
-          Follow Programming topic because Lorem ipsum dolor sit amet,
-          consectetur adipisicing elit. Accusamus nobis.
+          {props.info[0].description}
         </p>
       </div>
     </div>
