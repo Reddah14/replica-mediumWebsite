a98c1b11226b322880312225444256eafabe4cde
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-13 16:22:53 +0200

    adding separated folder for mock data

---
+++ src/data/banner.js
@@ -0,0 +1,22 @@
+const bannerMockedInfo = [
+  {
+    title: "Programming",
+    subtitle: "The good, the bad, the buggy.",
+    description:
+      "Follow to see more stories about Programming on your homepage and in your Medium Daily Digest",
+  },
+  {
+    title: "Data Science",
+    subtitle: "Query this.",
+    description:
+      "Follow to see more stories about Data Science on your homepage and in your Medium Daily Digest",
+  },
+  {
+    title: "UX",
+    subtitle: "All about that user.",
+    description:
+      "Follow to see more stories about UX on your homepage and in your Medium Daily Digest",
+  },    
+];
+
+export default bannerMockedInfo;
---
+++ src/data/news.js
@@ -0,0 +1,53 @@
+const newsMockedInfo = [
+  {
+    id: "e0",
+    title: "Map, Filter, and Reduce in Swift",
+    description:
+      "How to use your curiosity and Chrome DevTools to learn more about web...",
+    author: "Margarita Fox",
+    imageURL: "http://www.fillmurray.com/300/200",
+    readTime: 8,
+    date: "Set 8",
+  },
+  {
+    id: "e1",
+    title: "Write your tools: simple JIRA automation",
+    description:
+      "Cloud-based solutions are the new future for programmers and the data scientists...",
+    author: "Billie Jean",
+    imageURL: "https://www.placecage.com/c/140/200",
+    readTime: 4,
+    date: "Set 10",
+  },
+  {
+    id: "e2",
+    title: "Kickstart with Micronaut Framework",
+    description: "Fun Python programs explained with code...",
+    author: "Bruce Willis",
+    imageURL: "http://www.fillmurray.com/g/200/300",
+    readTime: 5,
+    date: "Set 11",
+  },
+  {
+    id: "e3",
+    title: "6 Must-try Python Programse",
+    description:
+      "It’s important not to put all your eggs into one basket. That’s why despite your main job it’s crucial you have a side gig. In this...",
+    author: "Madonna Smith",
+    imageURL: "https://placebear.com/200/300",
+    readTime: 3,
+    date: "Set 11",
+  },
+  {
+    id: "e4",
+    title: "How to execute an object file: Part 3",
+    description:
+      "It’s been more than 11 years in my career in the IT industry. I have worked in different teams of different sizes and of different...",
+    author: "John Doe",
+    imageURL: "https://www.stevensegallery.com/g/200/300",
+    readTime: 7,
+    date: "Set 12",
+  },
+];
+
+export default newsMockedInfo;
